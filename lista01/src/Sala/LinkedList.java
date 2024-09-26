464732ed3c06a2ff3fc8e95e6d0e0695a3addfad
202219050740 <202219050740@lab03-l>
2024-09-26 10:44:37 -0400

    vinculando ao github, versão zerada!

---
+++ lista01/src/Sala/LinkedList.java
@@ -0,0 +1,28 @@
+package Sala;
+
+public class LinkedList<T> {
+    int size;
+	Node<T> head;
+	
+	public LinkedList() {
+		this.size = 0;
+		this.head = null;
+	}
+	
+	public LinkedList(Node<T> head){
+		this.size = 1;
+		this.head = head;
+	}
+	
+	public void insert(T newValue) {
+		Node<T> newNode = new Node<T>(newValue);
+		newNode.next = head;
+		head = newNode;
+	}
+	
+	public T remove() {
+		T value = head.value;
+		head = head.next;
+		return value;
+	}
+}
